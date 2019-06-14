14d9c9363d8fc9fbd74197702989bc44a6b0b03b
Benedict Hauck <benedict.hauck@web.de>
2019-06-14 08:21:25 +0200

    Initial commit
    Problems: Hello World and Pot

--- Kattis_Pot/Program.cs
+++ Kattis_Pot/Program.cs
@@ -6,7 +6,24 @@ namespace Kattis_Pot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line;
+            long sum = 0;
+
+            //Console.WriteLine("The sum consists of " + Console.ReadLine() + " operands");
+            Console.ReadLine();
+
+            while ((line = Console.ReadLine()) != null)
+            {
+                sum += pow(Int64.Parse(line.Substring(0, line.Length - 1)), int.Parse(line.Substring(line.Length - 1, 1)));
+            }
+            Console.WriteLine(sum);
+        }
+        
+        static long pow(long b, int e)
+        {
+            if (e == 0) return 1;
+
+            return b * pow(b, e - 1);
         }
     }
 }
